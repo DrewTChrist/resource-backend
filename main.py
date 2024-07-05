c1d82e6d1ba8d320be797ad184d28bb75ba3e137
Andrew Christiansen <andrewtaylorchristiansen@gmail.com>
2024-07-04 20:07:56 -0700

    add CORS configuration

--- main.py
+++ main.py
@@ -2,7 +2,21 @@ from typing import Union
 
 from fastapi import FastAPI
 
+from fastapi.middleware.cors import CORSMiddleware
+
+origins = [
+    "https://sturdy-xylophone-4jvrqw47qv7fjxgw-5173.app.github.dev/"
+]
+
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.get("/")
