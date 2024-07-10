facc9e70e9fbbc61f5182322ff095fb308fc3352
Andrew Christiansen <andrewtaylorchristiansen@gmail.com>
2024-07-10 09:52:13 -0700

    Add resources and route for testing

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
 from typing import Union
-
 from fastapi import FastAPI
-
 from fastapi.middleware.cors import CORSMiddleware
+from resources import RESOURCES
 
 origins = [
     "https://sturdy-xylophone-4jvrqw47qv7fjxgw-5173.app.github.dev"
@@ -24,9 +23,15 @@ def read_root():
     return {"Hello": "World"}
 
 
+@app.get("/api/resources")
+def read_resources():
+    return RESOURCES
+
+
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
+
 
 if __name__ == '__main__':
     import uvicorn
