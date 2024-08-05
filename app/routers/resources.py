6848ac75e6ef6b4745718b326d2169de4f28d136
Andrew Christiansen <andrewtaylorchristiansen@gmail.com>
2024-08-05 12:16:51 -0700

    Add stream endpoint

--- app/routers/resources.py
+++ app/routers/resources.py
@@ -29,3 +29,11 @@ def read_resource(resource_id: int) -> models.Resource:
     if not resource:
         raise HTTPException(status_code=404, detail="Resource not found")
     return resource
+
+
+@router.get("/stream/{resource_id}")
+def stream_resource(resource_id: int):
+    resource = resources.get_resource(resource_id)
+    if not resource:
+        raise HTTPException(status_code=404, detail="Resource not found")
+    return StreamingResponse(iter_file(resource.path), media_type="video/mp4")
