404c81d745b0c65247a23884392f26b4c1037cd4
Andrew Christiansen <andrewtaylorchristiansen@gmail.com>
2024-08-06 15:00:27 +0000

    Send streaming content in chunks

--- app/routers/resources.py
+++ app/routers/resources.py
@@ -1,4 +1,7 @@
-from fastapi import APIRouter, Depends, HTTPException
+import os
+from typing import BinaryIO
+
+from fastapi import APIRouter, Depends, HTTPException, Request, status
 from fastapi.responses import StreamingResponse
 
 from app.internal.db import resources
@@ -32,9 +35,90 @@ def read_resource(resource_id: int) -> models.Resource:
     return resource
 
 
+def send_bytes_range_requests(
+    file_obj: BinaryIO, start: int, end: int, chunk_size: int = 10_000
+):
+    """Send a file in chunks using Range Requests specification RFC7233
+
+    `start` and `end` parameters are inclusive due to specification
+    """
+    with file_obj as f:
+        f.seek(start)
+        while (pos := f.tell()) <= end:
+            read_size = min(chunk_size, end + 1 - pos)
+            yield f.read(read_size)
+
+
+def _get_range_header(range_header: str, file_size: int) -> tuple[int, int]:
+    def _invalid_range():
+        return HTTPException(
+            status.HTTP_416_REQUESTED_RANGE_NOT_SATISFIABLE,
+            detail=f"Invalid request range (Range:{range_header!r})",
+        )
+
+    try:
+        h = range_header.replace("bytes=", "").split("-")
+        start = int(h[0]) if h[0] != "" else 0
+        end = int(h[1]) if h[1] != "" else file_size - 1
+    except ValueError:
+        raise _invalid_range()
+
+    if start > end or start < 0 or end > file_size - 1:
+        raise _invalid_range()
+    return start, end
+
+
+def range_requests_response(request: Request, file_path: str, content_type: str):
+    """Returns StreamingResponse using Range Requests of a given file"""
+
+    file_size = os.stat(file_path).st_size
+    range_header = request.headers.get("range")
+
+    headers = {
+        "content-type": content_type,
+        "accept-ranges": "bytes",
+        "content-encoding": "identity",
+        "content-length": str(file_size),
+        "access-control-expose-headers": (
+            "content-type, accept-ranges, content-length, "
+            "content-range, content-encoding"
+        ),
+    }
+    start = 0
+    end = file_size - 1
+    status_code = status.HTTP_200_OK
+
+    if range_header is not None:
+        start, end = _get_range_header(range_header, file_size)
+        size = end - start + 1
+        headers["content-length"] = str(size)
+        headers["content-range"] = f"bytes {start}-{end}/{file_size}"
+        status_code = status.HTTP_206_PARTIAL_CONTENT
+
+    return StreamingResponse(
+        send_bytes_range_requests(open(file_path, mode="rb"), start, end),
+        headers=headers,
+        status_code=status_code,
+    )
+
+
+# https://github.com/fastapi/fastapi/discussions/7718
+# https://stackoverflow.com/questions/68031189/fetching-videomp4-format-using-axios-in-react-and-displaying-in-html-video
 @router.get("/stream/{resource_id}")
-def stream_resource(resource_id: int):
+def stream_resource(resource_id: int, request: Request):
     resource = resources.get_resource(resource_id)
     if not resource:
         raise HTTPException(status_code=404, detail="Resource not found")
-    return StreamingResponse(iter_file(resource.path), media_type="video/mp4")
+    return range_requests_response(
+        request, file_path=resource.path, content_type="video/mp4"
+    )
+    # return StreamingResponse(
+    #     iter_file(resource.path),
+    #     status_code=206,
+    #     media_type="video/mp4",
+    #     headers={
+    #         # "Accept-Ranges": "bytes",
+    #         "Content-Length": str(2299653),
+    #         # "Content-Range": F"bytes {asked}-{sz-1}/{sz}",
+    #     },
+    # )
