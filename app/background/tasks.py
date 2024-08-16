c05bf098b405b8368775f24f66e0f27106798585
Andrew Christiansen <andrewtaylorchristiansen@gmail.com>
2024-08-16 11:16:02 -0700

    Finish index task function

--- app/background/tasks.py
+++ app/background/tasks.py
@@ -5,6 +5,7 @@ import pathlib
 from celery import Celery
 from app.internal import configuration
 from app.internal import models
+from app.internal.db import resources
 
 
 celery_instance = Celery(__name__)
@@ -12,7 +13,7 @@ celery_instance.conf.broker_url = configuration.get_config().celery_broker_url
 celery_instance.conf.result_backend = configuration.get_config().celery_result_backend
 
 
-def get_file_list(directory: str) -> list[ResourceFile]:
+def get_file_list(directory: str) -> list[models.Resource]:
     path = pathlib.Path(directory)
     resources = []
     for file in path.iterdir():
@@ -30,6 +31,7 @@ def get_file_list(directory: str) -> list[ResourceFile]:
 
 @celery_instance.task(name="index_files")
 def index_files():
-    # directory = configuration.get_config().resource_directory
-    # file_list = get_file_list(directory)
-    return True
+    directory = configuration.get_config().resource_directory
+    file_list = get_file_list(directory)
+    for file in file_list:
+        resources.create_resource(file)
