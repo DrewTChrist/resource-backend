36885da0f3c026f65ebc2750031b06e2c875dd96
Andrew Christiansen <andrewtaylorchristiansen@gmail.com>
2024-09-26 05:20:44 +0000

    Change user table to users

--- scripts/create_table.py
+++ scripts/create_table.py
@@ -10,7 +10,7 @@ CONNECTION_STRING = os.getenv("db_url")
 def create_table():
     connection = psycopg2.connect(CONNECTION_STRING)
     cursor = connection.cursor()
-    cursor.execute("CREATE TABLE user(id SERIAL PRIMARY KEY, first_name TEXT NOT NULL);")
+    cursor.execute("CREATE TABLE users(id SERIAL PRIMARY KEY, first_name TEXT NOT NULL);")
     connection.commit()
     cursor.close()
     connection.close()
