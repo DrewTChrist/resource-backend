1460a90e89e08c22d3d10ae33633776450ea04fa
Andrew Christiansen <andrewtaylorchristiansen@gmail.com>
2024-09-26 15:11:18 -0700

    Add sql for test users

---
+++ sql/test_data.sql
@@ -0,0 +1,50 @@
+/* admin */
+INSERT INTO users (
+    first_name, 
+    last_name, 
+    username, 
+    password_hash, 
+    disabled, 
+    administrator
+) VALUES (
+    "admin", 
+    "admin", 
+    "admin", 
+    "$2b$12$ta.KupNchm/CPN8Op9mY5.uai237aSMbc8jft3z..6zWiEOUb3f3e", --- "admin"
+    false, 
+    true
+);
+
+/* disabled user */
+INSERT INTO users (
+    first_name, 
+    last_name, 
+    username, 
+    password_hash, 
+    disabled, 
+    administrator
+) VALUES (
+    "disabled", 
+    "disabled", 
+    "disabled", 
+    "$2b$12$ta.KupNchm/CPN8Op9mY5.uai237aSMbc8jft3z..6zWiEOUb3f3e", --- "admin"
+    true, 
+    false
+);
+
+/* regular user */
+INSERT INTO users (
+    first_name, 
+    last_name, 
+    username, 
+    password_hash, 
+    disabled, 
+    administrator
+) VALUES (
+    "user", 
+    "user", 
+    "user", 
+    "$2b$12$ta.KupNchm/CPN8Op9mY5.uai237aSMbc8jft3z..6zWiEOUb3f3e", --- "admin"
+    false, 
+    false
+);
