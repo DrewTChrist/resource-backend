d3fc9c3524721e6f1fab213dc98b376458293486
Andrew Christiansen <andrewtaylorchristiansen@gmail.com>
2024-09-27 09:18:25 -0700

    Add test for user route

--- tests/test_users.py
+++ tests/test_users.py
@@ -5,5 +5,26 @@ from main import app
 client = TestClient(app)
 
 
-def test_one():
-    assert True == True
+def authenticate(username: str, password: str) -> str:
+    response = client.post(
+        "/api/auth/token",
+        headers={"Content-Type": "application/x-www-form-urlencoded"},
+        data={"username": username, "password": password},
+    )
+    return response.json()["access_token"]
+
+
+def test_get_current_user():
+    access_token = authenticate("admin", "admin")
+    response = client.get(
+        "/api/users/me", headers={"Authorization": f"Bearer {access_token}"}
+    )
+    json = response.json()
+    assert json == {
+        "user_id": 0,
+        "first_name": "admin",
+        "last_name": "admin",
+        "username": "admin",
+        "disabled": False,
+        "admin": True,
+    }
