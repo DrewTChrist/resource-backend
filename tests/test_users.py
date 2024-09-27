d8d0cc5a71d93050d541a1ab828d9ff2f0f3ca5e
Andrew Christiansen <andrewtaylorchristiansen@gmail.com>
2024-09-27 11:07:44 -0700

    Add more tests for user routes

--- tests/test_users.py
+++ tests/test_users.py
@@ -6,6 +6,7 @@ client = TestClient(app)
 
 
 def authenticate(username: str, password: str) -> str:
+    """Return an access token for use with tests"""
     response = client.post(
         "/api/auth/token",
         headers={"Content-Type": "application/x-www-form-urlencoded"},
@@ -14,7 +15,44 @@ def authenticate(username: str, password: str) -> str:
     return response.json()["access_token"]
 
 
+def test_get_users():
+    """Test the route for fetching all users
+
+    Any administrator should be able to access this route.
+    """
+    access_token = authenticate("admin", "admin")
+    response = client.get(
+        "/api/users/", headers={"Authorization": f"Bearer {access_token}"}
+    )
+    json = response.json()
+    assert len(json) == 3
+    assert json[0] == {
+        "user_id": 1,
+        "first_name": "admin",
+        "last_name": "admin",
+        "username": "admin",
+        "disabled": False,
+        "admin": True,
+    }
+
+
+def test_get_users_failure():
+    """Test the route for fetching all users
+
+    This test expects a failure from the endpoint. Regular users
+    will receive an error.
+    """
+    access_token = authenticate("user", "user")
+    response = client.get(
+        "/api/users/", headers={"Authorization": f"Bearer {access_token}"}
+    )
+    json = response.json()
+    assert response.status_code == 403
+    assert json == {"detail": "Insufficient permissions"}
+
+
 def test_get_current_user():
+    """Test route for fetching current user"""
     access_token = authenticate("admin", "admin")
     response = client.get(
         "/api/users/me", headers={"Authorization": f"Bearer {access_token}"}
@@ -28,3 +66,15 @@ def test_get_current_user():
         "disabled": False,
         "admin": True,
     }
+
+
+def test_get_current_user_failure():
+    """Test route for fetching current user
+
+    This test expects a failure from the endpoint. There is no access token
+    sent in the request so no user is authenticated.
+    """
+    response = client.get("/api/users/me")
+    json = response.json()
+    assert response.status_code == 401
+    assert json == {"detail": "Not authenticated"}
